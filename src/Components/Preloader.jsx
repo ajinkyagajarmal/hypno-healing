3f311ced82fc559f0462db75c11e2763beff1559
ajinkyagajarmal <ajinkya.gajarmal@gmail.com>
2025-07-22 17:38:44 +0530

    preloader logo added

--- src/Components/Preloader.jsx
+++ src/Components/Preloader.jsx
@@ -1,10 +1,16 @@
 // src/Components/Preloader.jsx
 import React from 'react';
 import styles from './Preloader.module.css';
+import logoImage from '../Assets/hypnologo.webp';
 
 const Preloader = ({ slogan }) => {
   return (
     <div className={styles.preloaderScreen}>
+      <img 
+        src={logoImage} 
+        alt="Company Logo" 
+        className={styles.logo} 
+      />
       <div className={styles.loader}></div>
       {slogan && <h1 className={styles.loadingText}>{slogan}</h1>}
     </div>
